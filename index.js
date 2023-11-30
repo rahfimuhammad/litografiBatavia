06bb3cf8fcf75be727b0035b7e032a0f41d0013b
rahfimuhammad <116084891+rahfimuhammad@users.noreply.github.com>
2023-12-01 00:19:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
+const express = require('express');
+const cors = require('cors');
 const jsonServer = require('json-server');
-const corsConfig = require('./cors-config'); // Impor konfigurasi CORS
+const server = express();
 
-const server = jsonServer.create();
+// Mengizinkan permintaan dari semua domain (ini mungkin tidak cocok untuk produksi)
+server.use(cors());
+
+// Gunakan router JSON Server untuk mengelola rute
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
-server.use(jsonServer.bodyParser);
-server.use(jsonServer.rewriter(require('./routes.json')));
+// Tambahkan middleware untuk menangani permintaan ke /museum/:id/reviews
+server.use('/museum/:id/reviews', (req, res, next) => {
+  // Dapatkan id museum dari parameter URL
+  const museumId = parseInt(req.params.id);
 
-// Gunakan middleware CORS dari file konfigurasi
-server.use(require('cors')(corsConfig));
+  // Ambil data dari db.json
+  const museums = router.db.get('museum').value();
 
-server.use(middlewares);
+  // Cari museum dengan id yang sesuai
+  const museum = museums.find((m) => m.id === museumId);
+
+  if (museum) {
+    // Jika museum ditemukan, kirim ulasan
+    res.json(museum.reviews || []);
+  } else {
+    // Jika museum tidak ditemukan, kirim respons dengan status 404
+    res.status(404).json({ message: 'Museum not found' });
+  }
+});
+
+// Gunakan router JSON Server untuk rute lainnya
 server.use(router);
 
-const port = process.env.PORT || 3000;
+// Port server
+const port = 3000;
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
